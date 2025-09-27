159fd9f838437b21541725e9365647cd0d5ff34e
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-09-27 10:27:13 +0900

    test

---
+++ src/domain/plan/core/plan.ts
@@ -0,0 +1,8 @@
+import { Customer } from "domain/customer";
+import { CinemaDate } from "domain/date";
+import { Price } from "domain/pricing";
+export interface Plan {
+    planName(): string;
+    isAvailable(customer: Customer): boolean;
+    price(cinemaDate: CinemaDate): Price;
+}
